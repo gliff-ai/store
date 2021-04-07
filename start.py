e46432658be6344570f53fc1aa4e42eb00a1ce86
Craig Cooper <c@c9r.dev>
2021-04-07 21:12:18 +0100

    fix: start.py

---
+++ start.py
@@ -0,0 +1,16 @@
+import os
+import uvicorn
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "craig_etebase_server.settings")
+
+django.setup()
+
+from django.core.asgi import get_asgi_application
+from etebase_fastapi.main import create_application
+
+if __name__ == "__main__":
+    django_application = get_asgi_application()
+
+    app = create_application()
+    uvicorn.run(app, host="0.0.0.0", port=8033)
