17837b6fbe6954a3074d82e4aeea1663ba04a041
Craig Cooper <c@c9r.dev>
2021-04-08 09:27:28 +0100

    fix: tidy up

---
+++ server/asgi.py
@@ -0,0 +1,16 @@
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+django_application = get_asgi_application()
+
+def create_app():
+    from etebase_fastapi.main import create_application
+    app = create_application()
+    app.mount("/etebase", django_application)
+
+    return app
+
+
+application = create_app()
