af866d86f205b55dad255d792d1e2f4154ea8891
Craig Cooper <c@c9r.dev>
2021-04-15 14:44:48 +0100

    fix: switch to postgres

---
+++ server/settings/staging.py
@@ -0,0 +1,13 @@
+from .base import *
+
+SECRET_KEY = get_env_value('SECRET_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': get_env_value('POSTGRES_NAME'),
+        'HOST': get_env_value('POSTGRES_HOST'),
+        'PASSWORD': get_env_value('POSTGRES_PASSWORD'),
+        'PORT': 5432,
+    }
+}
