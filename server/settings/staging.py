3bbeb3602df761d513620b801d6769d9fbe51b33
Craig Cooper <c@c9r.dev>
2021-04-15 15:00:17 +0100

    fix: name env var correctly

--- server/settings/staging.py
+++ server/settings/staging.py
@@ -5,7 +5,7 @@ SECRET_KEY = get_env_value('SECRET_KEY')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': get_env_value('POSTGRES_NAME'),
+        'NAME': get_env_value('POSTGRES_DATABASE'),
         'HOST': get_env_value('POSTGRES_HOST'),
         'PASSWORD': get_env_value('POSTGRES_PASSWORD'),
         'PORT': 5432,
