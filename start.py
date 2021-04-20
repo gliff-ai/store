4d15298abc8affcfc21f53130c920169e93d74a0
Craig Cooper <c@c9r.dev>
2021-04-20 19:09:11 +0100

    feat: setup asgi, update README

--- start.py
+++ start.py
@@ -6,25 +6,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings.base")
 
 django.setup()
 
-from django.core.asgi import get_asgi_application
-from etebase_fastapi.main import create_application
-
-from fastapi import APIRouter, Depends, status, Request
-from fastapi.security.api_key import APIKeyHeader
-
-from etebase_fastapi.dependencies import get_authenticated_user
-from myauth.models import UserType
-
-token_scheme = APIKeyHeader(name="Authorization")
+from server.asgi import get_application
 
 if __name__ == "__main__":
-    django_application = get_asgi_application()
-
-    app = create_application()
-
-    @app.get("/")
-    def root(request: Request, user: UserType = Depends(get_authenticated_user)):
-        return {"message": "Hello " + user.email}
-
+    app = get_application()
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
