36304832339eb0638d9532006f2ed3596a21d01a
Craig Cooper <c@c9r.dev>
2021-05-10 16:31:10 +0100

    feat: use Azure blob storage for media files

--- server/settings/staging.py
+++ server/settings/staging.py
@@ -1,6 +1,6 @@
 from .base import *
 
-SECRET_KEY = get_env_value('SECRET_KEY')
+SECRET_KEY = get_env_value("SECRET_KEY")
 
 
 # TODO:
@@ -9,18 +9,22 @@ CORS_ALLOW_CREDENTIALS = True
 # CORS_ORIGIN_WHITELIST = config("CORS_ORIGIN_WHITELIST").split(",")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': get_env_value('POSTGRES_DATABASE'),
-        'HOST': get_env_value('POSTGRES_HOST'),
-        'PASSWORD': get_env_value('POSTGRES_PASSWORD'),
-        'USER': get_env_value('POSTGRES_USER'),
-        'PORT': 5432,
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": get_env_value("POSTGRES_DATABASE"),
+        "HOST": get_env_value("POSTGRES_HOST"),
+        "PASSWORD": get_env_value("POSTGRES_PASSWORD"),
+        "USER": get_env_value("POSTGRES_USER"),
+        "PORT": 5432,
     }
 }
 
 STATIC_URL = "/static/"
 STATIC_ROOT = "/store_media/static"
 
-MEDIA_ROOT = os.environ.get("DJANGO_MEDIA_ROOT", "/media/")
-MEDIA_URL = "/store_media/media/"
+DEFAULT_FILE_STORAGE = "storages.backends.azure_storage.AzureStorage"
+
+AZURE_ACCOUNT_NAME = "staginggliff"
+AZURE_CONTAINER = "store-staging-media"
+AZURE_ACCOUNT_KEY = get_env_value("AZURE_ACCOUNT_KEY")
+AZURE_URL_EXPIRATION_SECS = 300
