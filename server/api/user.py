2059fb91e6c23cf561fafbef3034fff1edcb7148
Craig Cooper <c@c9r.dev>
2021-06-16 14:54:27 +0100

    fix: teams endpoint (#35)

--- server/api/user.py
+++ server/api/user.py
@@ -57,6 +57,7 @@ def create_user(request, payload: UserProfileIn):
 
     user.save()
     user_profile.id = user_profile.user_id  # The frontend expects id not user_id
+    user_profile.email = user.email
     return user_profile
 
 
