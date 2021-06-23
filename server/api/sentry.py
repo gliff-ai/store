8ef75906fc77a4cb36b28ab6a1a18006020e7e3d
Chas Nelson <github@chasnelson.co.uk>
2021-06-23 09:35:53 +0100

    fix: move auth=None inside the sentry router

--- server/api/sentry.py
+++ server/api/sentry.py
@@ -9,7 +9,7 @@ router = Router()
 
 
 # Submit a Sentry event
-@router.post("/", response={200: Error, 409: Error})
+@router.post("/", response={200: None, 409: Error}, auth=None)
 def post_event(request):
     logger.info("POST request on Sentry tunnel endpoint")
     logger.debug(request.body.decode("utf-8").split("\n"))
