53c05a9ce004698527fce3ff7217b12d53d31c1b
Craig Cooper <c@c9r.dev>
2021-06-29 11:42:17 +0100

    fix: nicer email templating, fix #55

---
+++ server/emails.py
@@ -0,0 +1,9 @@
+# A map of nice names for email templates -> sendgrid ids
+
+id = {
+    "invite_to_team": "d-4e62eee5c6b84a56b4225a2c3faa4c32",
+    "recover_account": "d-88b2e30576724d459416ea5dbd932ac7",
+    "collaborate_existing_user": "d-fc9bedae856e45a585a3d9c0950143ee",
+    "collaborate_new_user": "d-d8dbf68c17904ae9b8089d319c796d11",
+    "welcome": None,  # TODO
+}
