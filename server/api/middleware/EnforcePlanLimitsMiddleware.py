835e0055ae59e59d655271cca4fd7898d5ce4ab7
Craig Cooper <c@c9r.dev>
2021-08-04 14:01:36 +0100

    fix: tidier plan limits

--- server/api/middleware/EnforcePlanLimitsMiddleware.py
+++ server/api/middleware/EnforcePlanLimitsMiddleware.py
@@ -10,10 +10,10 @@ class EnforcePlanLimitsMiddleware:
         self.app = app
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        routes = ["/api/v1/collection/", "/django/api/invite/"]
+        routes = ["/api/v1/collection", "/django/api/user/invite"]
 
         # Do we care about this route?
-        if scope["path"] not in routes or scope["method"] != "POST":
+        if scope["method"] != "POST" or (not any(scope["path"].startswith(route) for route in routes)):
             await self.app(scope, receive, send)
             return
 
@@ -23,18 +23,25 @@ class EnforcePlanLimitsMiddleware:
         response = None
 
         # Projects
-        if scope["path"] == "/api/v1/collection/" and team_limits["projects_limit"] is not None:
+        if scope["path"].startswith("/api/v1/collection") and team_limits["projects_limit"] is not None:
             if team_limits["projects_limit"] >= team_limits["projects"]:
                 logger.info("Can't create a new project")
                 response = JSONResponse({"message": "Can't create a new project"}, status_code=401)
 
-        # Users & Collabs
-        if scope["path"] == "/django/api/invite/" and team_limits["users_limit"] is not None:
-            # TODO are they adding a user or a collaborator]
-
+        # Users
+        if scope["path"].startswith("/django/api/user/invite") and team_limits["users_limit"] is not None:
             if team_limits["users_limit"] >= team_limits["users"]:
                 logger.info("Can't invite a new user")
                 response = JSONResponse({"message": "Can't invite a new user"}, status_code=401)
+
+        # Collabs
+        if (
+            scope["path"].startswith("/django/api/user/invite/collaborator")
+            and team_limits["collaborators_limit"] is not None
+        ):
+            if team_limits["collaborators_limit"] >= team_limits["collaborators"]:
+                logger.info("Can't invite a new collaborator")
+                response = JSONResponse({"message": "Can't invite a new collaborator"}, status_code=401)
 
         if response is not None:
             await response(scope, receive, send)
