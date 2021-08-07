eb894ab901f7bd1393613fa52768fa72bac02cf6
Craig Cooper <c@c9r.dev>
2021-08-07 23:38:35 +0100

    fix: store all users storage under teams, send it to stripe

--- myauth/management/commands/update_team_storage_usage.py
+++ myauth/management/commands/update_team_storage_usage.py
@@ -1,9 +1,14 @@
+from datetime import datetime
+
 from loguru import logger
+import stripe
 from django.conf import settings
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore
 from django.core.management.base import BaseCommand
-from myauth.models import Team
+from myauth.models import Team, User, Billing, Tier
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 def get_container_client():
@@ -13,7 +18,23 @@ def get_container_client():
     return ContainerClient.from_connection_string(conn_str=connection_string, container_name=settings.AZURE_CONTAINER)
 
 
-def update_stripe_usage(customer_id, usage):
+def update_stripe_usage(subscription_id, storage_price_ids, usage):
+    # Get the storage price id
+    subscription = stripe.Subscription.retrieve(subscription_id)
+
+    price_id = next(
+        (item.id for item in subscription["items"]["data"] if item.price["id"] in storage_price_ids),
+        None,
+    )
+
+    if price_id:
+        stripe.SubscriptionItem.create_usage_record(
+            price_id,
+            quantity=usage,
+            timestamp=datetime.now(),
+        )
+    else:
+        logger.warning(f"User doesn't have a valid storage price set - {subscription_id}")
 
     return
 
@@ -33,6 +54,8 @@ def update_team_storage_usage():
     # necessary step because can iterate through blob_list only once
     data = {blob.name: blob.size for blob in blob_list}
 
+    logger.info("Got blob data")
+
     data_select = dict()
     for key, value in data.items():
         try:
@@ -46,14 +69,24 @@ def update_team_storage_usage():
 
     user_ids = data_select.keys()
 
-    for team in Team.objects.all():
-
-        user_id = str(team.id)
+    teams = dict()
+    for user in User.objects.all():
+        user_id = str(user.id)
         if user_id in user_ids:
-            # store usage in Mb and save team
-            team.usage = data_select[user_id] * 10 ** -6
-            team.save()
-            logger.info(f"Updated team {team.id}: new usage = {team.usage}")
+            # Add this users usage to their team usage
+            usage = data_select[user_id] * 10 ** -6
+            teams[user.team.id] = teams.get(user.team.id, 0) + usage
+            logger.info(f"Updated team {user.team.id} with {user_id}: new usage = {teams[user.team.id]}")
+
+    # Stripe Price IDs
+    price_ids = Tier.objects.all().values_list("stripe_storage_price_id", flat=True)
+
+    for key in teams:
+        team = Team.objects.get(id=key)
+        team.usage = teams[key]
+        team.save()
+        update_stripe_usage(team.billing.subscription_id, price_ids, team.usage)
+        # TODO check their limits?
 
 
 class Command(BaseCommand):
