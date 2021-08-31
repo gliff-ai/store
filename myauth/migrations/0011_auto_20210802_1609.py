865551759d801b873860a8ca3021effc63eb8f68
Chas Nelson <chas@gliff.ai>
2021-08-31 11:01:02 +0100

    Dependabot batch (#109)
    * build: Update minor python dependencies
    
    * build: Black

--- myauth/migrations/0011_auto_20210802_1609.py
+++ myauth/migrations/0011_auto_20210802_1609.py
@@ -6,13 +6,13 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('myauth', '0010_auto_20210730_0847'),
+        ("myauth", "0010_auto_20210730_0847"),
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='tier',
-            old_name='stripe_collaborator_project_id',
-            new_name='stripe_project_price_id',
+            model_name="tier",
+            old_name="stripe_collaborator_project_id",
+            new_name="stripe_project_price_id",
         ),
     ]
