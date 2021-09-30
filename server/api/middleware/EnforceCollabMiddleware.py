9444b8c9fabda7c366d3b1044a8c33ebff25f4a6
Silvia Zeni <silvia.szn@gmail.com>
2021-09-30 12:47:35 +0100

    fix: Allow collaborators to list and update store items

--- server/api/middleware/EnforceCollabMiddleware.py
+++ server/api/middleware/EnforceCollabMiddleware.py
@@ -4,6 +4,7 @@
 from loguru import logger
 from starlette.responses import JSONResponse
 from starlette.types import ASGIApp, Scope, Receive, Send
+import pdb
 
 from .helpers import get_key_from_headers, get_user_is_collab
 
@@ -15,7 +16,7 @@ class EnforceCollabMiddleware:
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         # [(Method, Path)]
         routes = [
-            ("POST", "/api/v1/collection"),  # Create Collection
+            ("POST", "/api/v1/collection"),  # Collections
             ("POST", "/django/api/invite"),  # Invite User
             ("GET", "/django/api/team"),  # View team
             ("*", "/django/api/billing"),  # Any billing routes
@@ -24,11 +25,19 @@ class EnforceCollabMiddleware:
         ]
 
         # Explicit Allow
-        if scope["path"].startswith("/django/api/billing/create-checkout-session") or scope["path"].startswith(
-            "/django/api/billing/webhook"
-        ):
-            await self.app(scope, receive, send)
-            return
+        allowed_routes = [
+            ("POST", "/django/api/billing/create-checkout-session"),
+            ("POST", "/django/api/billing/webhook"),
+            ("POST", "/api/v1/collection/list_multi/"),
+            ("POST", "/item/fetch_updates/"),
+            ("POST", "/item/transaction/"),
+            ("POST", "/item/batch/"),
+        ]
+
+        for (method, path) in allowed_routes:
+            if (method == scope["method"] or method == "*") and scope["path"].endswith(path):
+                await self.app(scope, receive, send)
+                return
 
         # Allow CORS requests
         if scope["method"] == "OPTIONS":
