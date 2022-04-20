73bc10b0cb454c0e7ed64dc9459dcea3fca15b8f
Craig Cooper <c@c9r.dev>
2022-04-20 10:05:33 +0700

    feat!: Billing v2 (#213)
    * test: tidy existing test
    
    * wip
    
    * feat: custom plans
    
    * feat: custom plans
    
    * feat: custom plans
    
    * build: add key to tests
    
    * build: lint
    
    * fix: test logs
    
    * fix: test logs
    
    * chore: format
    
    * fix: update readme
    
    * feat: changing plans
    
    * chore: tidy
    
    * chore: tidy

---
+++ __tests__/billing.js
@@ -0,0 +1,155 @@
+// These require STRIPE_SECRET_KEY to be set in the STORE instance
+import request from "supertest";
+import sqlite3 from "sqlite3";
+import { jest } from "@jest/globals";
+
+import Etebase, {
+  Account,
+  Collection,
+  CollectionManager,
+  Item,
+  ItemManager,
+  toBase64,
+  OutputFormat,
+  CollectionAccessLevel,
+} from "etebase";
+
+import { init, getNewEmail, BASE_URL, API_URL, ETEBASE_URL } from "./helpers";
+
+let db;
+let helpers;
+
+jest.setTimeout(10000);
+
+beforeAll(async () => {
+  await Etebase.ready;
+
+  db = new sqlite3.Database("../db.sqlite3", sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+  });
+
+  helpers = init(db);
+});
+
+describe.only("create a basic new user", () => {
+  let owner;
+
+  const cleanUser = async () => {
+    owner = await helpers.signup(getNewEmail());
+    await helpers.validate(owner.userReq, owner.userId);
+  };
+
+  beforeAll(async () => {
+    owner = await helpers.signup(`${Math.random()}@gliff.ai`);
+    await helpers.validate(owner.userReq, owner.userId);
+  }, 30000);
+
+  describe("billing", () => {
+    describe("free trial", () => {
+      it("user is on a small plan trial", async () => {
+        const { body } = await owner.userReq.get("/billing/plan").expect(200);
+        expect(body.tier_id).toBe(2);
+        expect(body.trial_end).toBeGreaterThan(body.current_period_start);
+      }, 30000);
+
+      it.skip("user can cancel trial", async () => {});
+
+      it("user has an invoice for 0", async () => {
+        const {
+          body: {
+            invoices: [invoice],
+          },
+        } = await owner.userReq.get("/billing/invoices").expect(200);
+
+        expect(invoice.amount_due).toEqual(0);
+        expect(invoice.paid).toEqual(true);
+      });
+
+      it("user can't addon (as they have no payment details)", async () => {
+        await owner.userReq
+          .post("/billing/addon/")
+          .send({ users: 3 })
+          .expect(422, { message: "No valid payment method" });
+      });
+
+      // TODO: Maybe test trial ending
+    });
+
+    describe("Standard Plans", () => {
+      // TODO: Maybe add the actual checkout process with selenium
+      it.skip("user create a checkout session to add a payment method", async () => {
+        const { body } = await owner.userReq
+          .post("/billing/create-checkout-session/")
+          .expect(200);
+
+        expect(body.id).toBeDefined();
+      });
+
+      it("user can downgrade to free plan", async () => {
+        const { body } = await owner.userReq
+          .post("/billing/plan/")
+          .send({ tier_id: 1 })
+          .expect(200);
+
+        expect(body.tier_id).toBe(1);
+
+        // Reset user
+        await cleanUser();
+      }, 30000);
+
+      it("user can't upgrade without payment", async () => {
+        await owner.userReq
+          .post("/billing/plan/")
+          .send({ tier_id: 3 })
+          .expect(422, { message: "No valid payment method" });
+
+        const { body } = await owner.userReq.get("/billing/plan").expect(200);
+        expect(body.tier_id).toBe(2);
+      });
+
+      it.skip("user can cancel", async () => {
+        //     await owner.userReq.post("/billing/cancel/").expect(422, {
+        // message: "No valid subscription to cancel. Try changing your plan",
+      });
+
+      it("user can't choose a lower plan than appropriate", async () => {
+        const email = getNewEmail();
+        const {
+          body: { id: invite_id },
+        } = await owner.userReq
+          .post("/user/invite/")
+          .send({ email })
+          .expect(200);
+
+        await helpers.signup(email, { team_id: owner.teamId, invite_id });
+        await owner.userReq
+          .post("/billing/plan/")
+          .send({ tier_id: 1 })
+          .expect(422, {
+            message: "Too many users to switch to this plan",
+          });
+
+        const { body } = await owner.userReq.get("/billing/plan").expect(200);
+        expect(body.tier_id).toBe(2);
+      }, 30000);
+    });
+
+    // This test requires a valid Stripe Subscription to run.
+    // You can use `stripe fixtures stripe_fixtures/create-fixtures.json` to add one, and then manually add the ID to the tiers table
+    describe.skip("Custom Plans", () => {
+      it("Can signup (once!) with a custom plan", async () => {
+        const custom = await helpers.signup(`${Math.random()}@gliff.ai`, {
+          tier_id: 10001,
+        });
+        await helpers.validate(custom.userReq, custom.userId);
+
+        const { body } = await custom.userReq.get("/billing/plan").expect(200);
+        expect(body.tier_id).toBe(10001);
+      }, 30000);
+    });
+
+    describe.skip("Coupons", () => {});
+  });
+});
