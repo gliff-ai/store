04b4987a42eac50032550f77c3870d5437de9a9f
Silvia Zeni <silvia.szn@gmail.com>
2022-07-22 09:37:33 +0100

    fix: Formatting

--- myauth/migrations/0037_userfeedback.py
+++ myauth/migrations/0037_userfeedback.py
@@ -8,18 +8,30 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('myauth', '0036_usage'),
+        ("myauth", "0036_usage"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='UserFeedback',
+            name="UserFeedback",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('rating', models.PositiveSmallIntegerField(blank=True, choices=[(4, 'excellent'), (3, 'very good'), (2, 'good'), (1, 'fair'), (0, 'poor')], null=True)),
-                ('comment', models.TextField(blank=True, max_length=500)),
-                ('date', models.DateTimeField(auto_now_add=True)),
-                ('user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
+                ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                (
+                    "rating",
+                    models.PositiveSmallIntegerField(
+                        blank=True,
+                        choices=[(4, "excellent"), (3, "very good"), (2, "good"), (1, "fair"), (0, "poor")],
+                        null=True,
+                    ),
+                ),
+                ("comment", models.TextField(blank=True, max_length=500)),
+                ("date", models.DateTimeField(auto_now_add=True)),
+                (
+                    "user",
+                    models.ForeignKey(
+                        null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL
+                    ),
+                ),
             ],
         ),
     ]
